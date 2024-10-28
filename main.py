6d3d41301aa4f89bf4cfd544d7e0d22c9ecc8a9e
Kajot-dev <jjaruszewski@outlook.com>
2024-10-29 00:19:31 +0100

    Initial attempt

--- main.py
+++ main.py
@@ -1,9 +1,102 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import re
+import requests
+import itertools
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+# regex patterns
+CATEGORY_ARTICLES = r'<li[^>]*>.*<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>.*</li>'
+ARTICLE_INNER_LINKS = r'<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
+ARTICLE_IMAGES = r'<img[^>]*src=\"(//upload\.wikimedia\.org/[^"]+)\"[^>]*/>'
+REF_LINKS = r'<a[^>]*class=\"external[^"]*\"[^>]*href=\"([^"]+)\"[^>]*>'
+CAT_LINKS = r'<a[^>]*href=\"(/wiki/Kategoria:[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+
+def pipe_join_print(iterable):
+    joined = ' | '.join(iterable)
+    print(joined)
+
+def get_article_body_text(html: str) -> str:
+    return html[html.find('<div id="mw-content-text"'):html.find('<div id="catlinks"')]
+
+
+def get_article_reflinks_text(html: str) -> str:
+    return html[html.find('<ol class="references">'):]
+
+
+def get_article_category_text(html: str) -> str:
+    return html[html.find('<div id="catlinks"'):]
+
+
+def findall_with_limit(pattern: str, text: str, flags: int = 0, limit: int = 5) -> list:
+    return list(map(lambda m: m.groups(), itertools.islice(re.finditer(pattern, text, flags=flags), limit)))
+
+
+def get_category_page_url(category: str) -> str:
+    parsed_category = category.replace(' ', '_')
+    return f'https://pl.wikipedia.org/wiki/Kategoria:{parsed_category}'
+
+
+def get_category_articles(category: str, limit: int = 3) -> list[tuple[str, str]]:
+    category_page_url = get_category_page_url(category)
+    # get raw html
+    response = requests.get(category_page_url)
+    html = response.text
+    # parse html
+    articles = findall_with_limit(CATEGORY_ARTICLES, html, limit=limit)
+
+    return articles
+
+
+def get_article(article_url: str) -> str:
+    response = requests.get("https://pl.wikipedia.org" + article_url)
+    html = response.text
+    return html
+
+
+def get_article_inner_links(article: str, limit: int = 5) -> list[tuple[str, str]]:
+    html = get_article_body_text(article)
+    links = findall_with_limit(ARTICLE_INNER_LINKS, html, limit=limit)
+    
+    return links
+
+
+def get_article_images(article: str, limit: int = 3) -> list:
+    html = get_article_body_text(article)
+    images = findall_with_limit(ARTICLE_IMAGES, html, limit=limit)
+    
+    return images
+
+
+def get_article_refs(article: str, limit: int = 3) -> list:
+    html = get_article_reflinks_text(article)
+    refs = findall_with_limit(REF_LINKS, html, limit=limit)
+    
+    return refs
+
+
+def get_article_categories(article: str, limit: int = 3) -> list:
+    html = get_article_category_text(article)
+    categories = findall_with_limit(CAT_LINKS, html, limit=limit)
+    
+    return categories
+
+
+def main():
+    cat = input().strip()
+    articles = get_category_articles(cat)
+    for article_link, article_title in articles:
+        article = get_article(article_link)
+        
+        links = get_article_inner_links(article)
+        pipe_join_print(map(lambda x: x[1], links))
+        
+        images = get_article_images(article)
+        pipe_join_print(map(lambda x: x[0], images))
+        
+        refs = get_article_refs(article)
+        pipe_join_print(map(lambda x: x[0], refs))
+        
+        categories = get_article_categories(article)
+        pipe_join_print(map(lambda x: x[1].removeprefix('Kategoria:'), categories))
+    
+if __name__ == '__main__':
+    main()
